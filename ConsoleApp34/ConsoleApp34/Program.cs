c5a3c781c3b4e1c9dd18e73137f1dbaf8f880870
TabrezGulobov <tabrezgulobov@gmail.com>
2024-12-06 18:26:16 +0500

    Add files via upload

---
+++ ConsoleApp34/ConsoleApp34/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp29
+{
+    class Pogram
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var proizvedenie = 1;
+            for (int i = a; i <= b; i++)
+            {
+                proizvedenie *= i;
+                Console.WriteLine(proizvedenie);
+            }
+        }
+    }
+}
